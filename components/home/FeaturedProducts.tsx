95167733a1025f1d162e5b907720fe10b7fe78ec
vinhtq1 <vinhtq1@runsystem.net>
2025-02-18 15:32:08 +0700

    fix

--- components/home/FeaturedProducts.tsx
+++ components/home/FeaturedProducts.tsx
@@ -6,7 +6,8 @@ import ProductsGrid from "../products/ProductsGrid";
 
 const FeaturedProducts = async () => {
   const products = await fetchFeatureProducts();
-  if (products.length === 0) return <EmptyList />;
+  if (products.length === 0)
+    return <EmptyList heading="No items found" className="text-emerald-50" />;
   return (
     <section className="pt-24">
       <SectionTitle text="featured products" />
