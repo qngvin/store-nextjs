bd08dcaca993d06b68521dc0eb19f9799eec4ff9
vinhtq1 <vinhtq1@runsystem.net>
2025-02-18 15:29:42 +0700

    fix more

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -3,7 +3,7 @@ import ProductsContainer from "@/components/products/ProductsContainer";
 async function ProductsPage({
   searchParams,
 }: {
-  searchParams: { layout?: string | undefined; search?: string };
+  searchParams: Promise<{ layout?: string | undefined; search?: string }>;
 }) {
   const resolvedSearchParams = await searchParams;
   const layout = resolvedSearchParams.layout || "grid";
